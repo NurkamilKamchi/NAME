b18fc40c5a625633914fcd827e191bee127e9741
nurkamil <nurkamilkamchiev@gmail.com>
2024-01-11 12:22:14 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,6 @@ import java.util.Scanner;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello team!");
-        System.out.println("Hello kyzdar!");
-        System.out.println();
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter name: ");
-        System.out.println("Nurkamil Kamchiev");
-        System.out.println("Aiturgan");
-        System.out.println();
+
     }
 }
